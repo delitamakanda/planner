27607feb8cd20ac85b60b29b66e20d45eadc0844
Délita Makanda <delita.makanda@gmail.com>
2026-02-28 00:48:23 +0100

    feat: add swagger

--- api/schema_urls.py
+++ api/schema_urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
